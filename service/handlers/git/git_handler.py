bd5bcf3f201398035d739d8ebfa876f011b409a4
Slim <zwigoda99@gmail.com>
2020-01-16 19:03:05 +0200

    - Minor fixes for potential bugs

--- service/handlers/git/git_handler.py
+++ service/handlers/git/git_handler.py
@@ -1,4 +1,5 @@
 from git import Repo
+import os
 
 
 class GitHandler(object):
@@ -8,5 +9,7 @@ class GitHandler(object):
         self._target_location = target_location
 
     def clone(self):
+        if not os.path.exists(self._target_location):
+            os.makedirs(self._target_location)
         repo = Repo.clone_from(url=self._url, to_path=self._target_location, branch=self._branch)
         return self._target_location
